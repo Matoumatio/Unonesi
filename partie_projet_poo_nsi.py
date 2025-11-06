6d3f2ce7b3fe6aef4d3cf88169a00384361c9ce7
tomf09 <falzonetom30@gmail.com>
2025-11-06 15:53:14 +0100

    ajout partie

---
+++ partie_projet_poo_nsi.py
@@ -0,0 +1,10 @@
+#coder les mecaniques de jeu et les regle en poo
+
+#Travail sur le fonctionnement du jeu (resume : en gros)
+
+#si dans le paquet (utiliser fifo) Exemple : si dans le paquet ya 7 jaune la personne est oblige de jouer une carte jaune ou un 7
+#voir reste des regle puis faire pareil
+
+#Regles:
+#voir regle site (toutes sauf les trois dernieres) regles classique 
+# (regle : si on pioche une bonne carte, on peut rejouer direct, si on se prend une sanction et qu'on pioche on ne peut pas rejouer) 
