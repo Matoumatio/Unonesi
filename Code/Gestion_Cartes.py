f55fd79a9fda7440f113bcd21c50963919133bff
Lucy16170 <s.zimmermann0408@gmail.com>
2025-11-19 09:46:26 +0100

    correction

--- Code/Gestion_Cartes.py
+++ Code/Gestion_Cartes.py
@@ -1,5 +1,5 @@
 import random
-class cartes:
+class carte:
         def __init__(self, couleur, valeur):
            self.couleur = couleur   # 'rouge', 'jaune', 'vert', 'bleu' ou 'noir' (joker)
            self.valeur = valeur     # 0-9, 'inverse', 'passe', '+2', 'joker', '+4'
@@ -19,14 +19,14 @@ class JeuDeCartes:
         # Cartes de couleur
         for couleur in couleurs:
             for valeur in valeurs:
-                jeu.append(Carte(couleur, valeur))
+                jeu.append(carte(couleur, valeur))
                 if valeur != 0:
-                    jeu.append(Carte(couleur, valeur))  # deux exemplaires sauf 0
+                    jeu.append(carte(couleur, valeur))  # deux exemplaires sauf 0
 
         # Jokers
         for _ in range(4):
-            jeu.append(Carte('noir', 'joker'))
-            jeu.append(Carte('noir', '+4'))
+            jeu.append(carte('noir', 'joker'))
+            jeu.append(carte('noir', '+4'))
 
         random.shuffle(jeu)
         return jeu
