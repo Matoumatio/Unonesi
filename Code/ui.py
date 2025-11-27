42cdd00d3091064402288896cb4dafd167dcf412
Matoumatio <125093850+Matoumatio@users.noreply.github.com>
2025-11-27 14:40:41 +0100

    Ajout de ui.py et du fichier constants.json

---
+++ Code/ui.py
@@ -0,0 +1,9 @@
+# ui.py
+# Module de gestion de l'interface graphique avec Arcade
+
+import arcade
+from arcade import Sprite, SpriteList
+from typing import List, Tuple
+
+import constants.json as constants
+
